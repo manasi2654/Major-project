cf282c34af823d1df313c4cddcdc5578f830ae22
manasi2654 <145982061+manasi2654@users.noreply.github.com>
2025-04-26 15:33:18 +0530

    Update App.js

--- src/react/app.js
+++ src/react/app.js
@@ -1,32 +1,29 @@
-
-
-import React, { useState } from "react";
-import AuthPage from "./AuthPage"; // Import the AuthPage component
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "antd/dist/antd.css";
+import Router from "./routes/Router";
+import { factoryABI } from "./abi";
+import { ethers } from "ethers";
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [userAddress, setUserAddress] = useState("");
+  const [contract, setContract] = useState(null);
 
-  // Function to handle authentication
-  const handleAuthentication = (address) => {
-    setAuthenticated(true);
-    setUserAddress(address);
-  };
+  useEffect(() => {
+    const provider = ethers.getDefaultProvider("sepolia");
+
+    const contract = new ethers.Contract(
+      "0xd8e1dA4F2447Ce548A0E76374A0eFE625B48840B",
+      factoryABI,
+      provider
+    );
+    setContract(contract);
+  }, []);
 
   return (
-    <div className="App">
-      {!authenticated ? (
-        <AuthPage onAuthenticated={handleAuthentication} />
-      ) : (
-        <div>
-          <h1>Welcome to the Crowdfunding DApp</h1>
-          <p>Your wallet is connected with address: {userAddress}</p>
-          {/* The rest of your DApp features go here */}
-        </div>
-      )}
+    <div className='App'>
+      {/* <button onClick={fetch}>setmaster</button> */}
+      <Router contract={contract} />
     </div>
   );
 }
 
 export default App;
-
